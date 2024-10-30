cd3a3e27556b88211f67754a75c705b4e7d81c75
Aliyah Moore <aliyahm.moore@yahoo.com>
2024-10-30 07:04:20 -0500

    loops in class

---
+++ ruby/classwork/loops-and-iterators/script.rb
@@ -0,0 +1,16 @@
+for num in 1..20
+    if num % 2 == 0
+    puts "#{num} is even"
+    else
+        puts "#{num} is odd"
+    end 
+end
+
+num = 1
+while num <= 20
+    for num in 1..20
+        if num %2 = 0
+        puts "#{num is even}"
+    else
+        puts "#{num is odd}"
+end
